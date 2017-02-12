4466a64a11e6741924035fd9acff4ba2ac1d7e7e
Man Yiu Lam <manyiulam97@gmail.com>
2017-02-12 17:46:08 +0000

    Update RandomArrayPrintMissingElements.java

--- RandomArrayPrintMissingElements.java
+++ RandomArrayPrintMissingElements.java
@@ -1,4 +1,4 @@
-public class RandomArrayPrintMissingElements {	// Print missing elements in this array!
+public class test {	// Print missing elements in this array!
 public static void main(String[]args) {
 	int[] array = {5,1,9,3,30,20,18};
 	int first = 0 , last = array.length-1 , temp = 0;
@@ -16,7 +16,7 @@ public static void main(String[]args) {
 		int start = 0 , fin = array.length-1;
 
 		while(start<fin) {						// Finding difference between i and i+1 and printing within a sorted array
-			for(int k=array[start]+1; k<array[fin]; k++) {
+			for(int k=array[start]+1; k<array[start+1]-1; k++) {
 				System.out.printf("%s ",k);
 				}
 				start++;
