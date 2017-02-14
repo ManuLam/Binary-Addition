896e8a091852f3b1d56375ab8eaf6fb9aa917d0c
Man Yiu Lam <manyiulam97@gmail.com>
2017-02-14 20:30:27 +0000

    Add files via upload

---
+++ NewLoops.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class NewLoops {
+public static void main(String[]args) {
+	String[] strArray = {"apple","banna","cat","dog","elephant"};
+
+	for(String x : strArray) {
+		System.out.printf("%s ",x);
+	}
+
+    }
+}
